010d2a7e74d38ed33f49921e57318866ddfad9ff
Chang <chang.rajani@gmail.com>
2015-03-02 22:41:54 +0200

    Added static file serving

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Ratebeer
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += Dir["#{Rails.root}/lib"]
+    config.serve_static_files = true
   end
 end
